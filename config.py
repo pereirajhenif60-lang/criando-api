60349e6b7c91e1b64e1edcbbcff814b1fd3842a9
jhenif pereira <pereirajhenif60@gmail.com>
2026-09-17 14:02:07 -0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    BASE_URL = "https://wttr.in"
-    DEFAULT_LANG = "pt"
-    GOIAS_CITIES = [
-        "Goiânia", "Iporá", "Anápolis", "Rio Verde",
-        "Águas Lindas de Goiás", "Itumbiara",
-        "Jataí", "Formosa", "Senador Canedo",
-        "Trindade"
-    ]
